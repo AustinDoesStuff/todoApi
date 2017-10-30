52496e0af33f51cc9058f513ab31ebd3fcae2ec9
Austin Tanner <austin.tanner04@gmail.com>
2017-10-30 15:12:23 -0500

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 3
-};
+var password = 'abc123';
 
-var token = jwt.sign(data, 'abc123');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+var hashedPassword = '$2a$10$g2z4gicLWIaI1Fim7CaG5us/0jQ.mjazG2VyYSa2PGxG1LXnEslta';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 3
+// };
+
+// var token = jwt.sign(data, 'abc123');
+
+// var decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
 
 // var message = 'dis a test';
 // var hash = SHA256(message).toString();
